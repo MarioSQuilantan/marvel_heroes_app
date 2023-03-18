7bbe585150a06c6a902a93434c868f3b345f22de
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-17 22:53:58 -0600

    feat: :sparkles: created character mock file

---
+++ test/features/characters/character_mocks.dart
@@ -0,0 +1,4 @@
+import 'package:marvel_heroes_app/features/characters/domain/repositories/characters_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockCharactersRepository extends Mock implements CharactersRepository {}
