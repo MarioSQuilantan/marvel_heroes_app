f80aeb1b0d2ae47ee1bdb810cd549dd863b3da4f
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-17 21:09:12 -0600

    feat: :sparkles: created ScaleSize code

--- lib/core/utils/scale_size.dart
+++ lib/core/utils/scale_size.dart
@@ -1,1 +1,12 @@
-// TODO(MarioSQuilantan): Implement scale size code.
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ScaleSize {
+  static double textScaleFactor(BuildContext context, {double maxTextScaleFactor = 1.8}) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    double value = (width / height) * maxTextScaleFactor;
+    return max(1, min(value, maxTextScaleFactor));
+  }
+}
