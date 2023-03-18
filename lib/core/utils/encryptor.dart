dc2bb0cbdcda536cc6cf83b4d3a8bd81e1262791
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-17 21:31:43 -0600

    feat: :sparkles: created encryptor code

--- lib/core/utils/encryptor.dart
+++ lib/core/utils/encryptor.dart
@@ -1,1 +1,13 @@
-// TODO(MarioSQuilantan): Implement encryptor code.
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+String encryptor(String publicKey, String privateKey, String timeStamp) {
+  String dataToHash = '$timeStamp$privateKey$publicKey';
+
+  List<int> bytesToHash = utf8.encode(dataToHash);
+  Digest digest = md5.convert(bytesToHash);
+
+  String hash = digest.toString();
+
+  return hash;
+}
