45342065a9782915806a68da22cc457014ede916
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-17 22:52:18 -0600

    feat: :sparkles: created failure class

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,1 +1,14 @@
-// TODO(MarioSQuilantan): Create Failure code.
+import 'package:equatable/equatable.dart';
+
+class Failure extends Equatable {
+  final String message;
+
+  const Failure(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+}
