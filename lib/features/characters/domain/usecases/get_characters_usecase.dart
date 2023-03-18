8f9a137500bd142d31a08b295b52945fabe838a1
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-17 22:58:31 -0600

    feat: :sparkles: created get characters use case class

---
+++ lib/features/characters/domain/usecases/get_characters_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+import 'package:marvel_heroes_app/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:marvel_heroes_app/core/interfaces/use_case.dart';
+import 'package:marvel_heroes_app/features/characters/domain/entities/character_entity.dart';
+import 'package:marvel_heroes_app/features/characters/domain/repositories/characters_repository.dart';
+
+class GetCharactersUseCase extends UseCase<Character, Params> {
+  final CharactersRepository charactersRepository;
+
+  GetCharactersUseCase(this.charactersRepository);
+
+  @override
+  Future<Either<Failure, Character>> call(Params params) => charactersRepository.getCharacters(params.offset);
+}
+
+class Params extends Equatable {
+  final int offset;
+
+  const Params({
+    required this.offset,
+  });
+
+  @override
+  List<Object?> get props => [offset];
+}
