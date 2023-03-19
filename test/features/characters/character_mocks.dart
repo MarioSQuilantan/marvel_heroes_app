ef6fe45ff7337492e1cde1dd741a516a54632069
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-18 18:25:17 -0600

    refactor: :zap: Added http and apiRemoteDataSource mocks

--- test/features/characters/character_mocks.dart
+++ test/features/characters/character_mocks.dart
@@ -1,4 +1,10 @@
+import 'package:marvel_heroes_app/features/characters/data/datasources/api_remote_datasource.dart';
 import 'package:marvel_heroes_app/features/characters/domain/repositories/characters_repository.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:http/http.dart' as http_client;
 
 class MockCharactersRepository extends Mock implements CharactersRepository {}
+
+class MockApiRemoteDataSource extends Mock implements ApiRemoteDataSource {}
+
+class MockHttp extends Mock implements http_client.Client {}
