c6b999475349563d9eb3dddf99764551fbbf12ba
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-19 10:23:06 -0600

    refactor: :zap: added dependency injection and bloc provider in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marvel_heroes_app/features/characters/presentation/bloc/characters_bloc.dart';
 
 import 'app_routes.dart';
 import 'core/theme/theme.dart';
+import 'injector.dart' as di;
 
 void main() {
+  di.init();
   runApp(const MyApp());
 }
 
@@ -12,11 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Marvel Heroes',
-      theme: theme,
-      routerConfig: AppRoutes.init,
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CharactersBloc>(create: (_) => di.locator<CharactersBloc>()),
+      ],
+      child: MaterialApp.router(
+        title: 'Marvel Heroes',
+        theme: theme,
+        routerConfig: AppRoutes.init,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
