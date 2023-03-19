dc98205cd6949cf2f2a45ae97fd4a7c364408b7e
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-19 10:24:24 -0600

    refactor: :zap: updated http client query params

--- lib/core/network/http_client.dart
+++ lib/core/network/http_client.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http_client;
+import 'package:marvel_heroes_app/core/values/constants.dart';
 
 import 'network_client.dart';
 
@@ -12,6 +13,24 @@ class HttpClient implements NetworkClient {
     String url, {
     Map<String, dynamic>? queryParameters,
   }) {
-    return http.get(Uri.parse(url).replace(queryParameters: queryParameters));
+    final baseQueryParameters = <String, dynamic>{
+      'apikey': ApiConstants.publicKey,
+      'hash': ApiConstants.hash,
+      'ts': ApiConstants.timeStamp,
+    };
+
+    if (queryParameters != null) {
+      baseQueryParameters.addAll(queryParameters);
+    }
+
+    final uri = Uri.parse(url).replace(
+      queryParameters: baseQueryParameters.map(
+        (key, value) => MapEntry(
+          key,
+          value.toString(),
+        ),
+      ),
+    );
+    return http.get(uri);
   }
 }
--- lib/core/values/constants.dart
+++ lib/core/values/constants.dart
@@ -6,10 +6,10 @@ class Routes {
 
 class ApiConstants {
   static const String _baseUrl = 'https://gateway.marvel.com/v1/public';
-  static const String _publicKey = 'fea4ddbf370376865724c2b03db5ffef';
-  static const String _privateKey = 'dcca949708ec8a3a3b97cf51cacf65cd98a314c4';
-  static String timeStamp = DateTime.now().millisecondsSinceEpoch.toString();
-  static String hash = encryptor(_publicKey, _privateKey, timeStamp);
+  static const String publicKey = 'fea4ddbf370376865724c2b03db5ffef';
+  static const String privateKey = 'dcca949708ec8a3a3b97cf51cacf65cd98a314c4';
+  static String get timeStamp => DateTime.now().millisecondsSinceEpoch.toString();
+  static String get hash => encryptor(publicKey, privateKey, timeStamp);
 
-  static String get getCharacters => '$_baseUrl/characters?apikey=$_publicKey&hash=$hash&ts=$timeStamp';
+  static String get getCharacters => '$_baseUrl/characters?apikey=$publicKey&hash=$hash&ts=$timeStamp';
 }
