fa0339c109e11446ff339992f5ef39a64f9a39a1
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-18 18:26:36 -0600

    feat: :sparkles: created api remote data source code

---
+++ lib/features/characters/data/datasources/api_remote_datasource.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import '../../../../core/error/exception.dart';
+import '../../../../core/network/network_client.dart';
+import '../../../../core/values/constants.dart';
+import '../models/character_model.dart';
+
+abstract class ApiRemoteDataSource {
+  Future<CharacterModel> getCharacters(int offset);
+}
+
+class ApiRemoteDataSourceImpl extends ApiRemoteDataSource {
+  final NetworkClient networkClient;
+
+  ApiRemoteDataSourceImpl(this.networkClient);
+
+  @override
+  Future<CharacterModel> getCharacters(int offset) async {
+    final queryParameters = {
+      'offset': '$offset',
+    };
+    final response = await networkClient.get(ApiConstants.getCharacters, queryParameters: queryParameters);
+    if (response.statusCode >= 200 && response.statusCode < 400) {
+      return CharacterModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+}
