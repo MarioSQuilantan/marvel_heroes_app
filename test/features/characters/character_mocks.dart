08e305304b66ffa0f48401ba8afd7588ffda1411
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-18 20:06:59 -0600

    refactor: :zap: Added get characters use case mock

--- test/features/characters/character_mocks.dart
+++ test/features/characters/character_mocks.dart
@@ -1,10 +1,13 @@
 import 'package:marvel_heroes_app/features/characters/data/datasources/api_remote_datasource.dart';
 import 'package:marvel_heroes_app/features/characters/domain/repositories/characters_repository.dart';
+import 'package:marvel_heroes_app/features/characters/domain/usecases/get_characters_usecase.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:http/http.dart' as http_client;
 
 class MockCharactersRepository extends Mock implements CharactersRepository {}
 
 class MockApiRemoteDataSource extends Mock implements ApiRemoteDataSource {}
+
+class MockGetCharactersUseCase extends Mock implements GetCharactersUseCase {}
 
 class MockHttp extends Mock implements http_client.Client {}
