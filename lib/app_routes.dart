37a7934abc557ae923e9c7cd35d0a8a0a968da07
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-19 23:37:43 -0600

    feat: :zap: Added character page route

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:marvel_heroes_app/features/characters/domain/entities/characters/result_entity.dart';
+import 'package:marvel_heroes_app/ui/pages/character/character_page.dart';
 
 import 'core/values/constants.dart';
 import 'ui/pages/home/home_page.dart';
@@ -12,8 +14,19 @@ class AppRoutes {
             path: Routes.home,
             builder: (context, state) => const HomePage(),
           ),
+          GoRoute(
+            path: Routes.character,
+            builder: (context, state) {
+              Result result = state.extra as Result;
+              return CharacterPage(
+                result: result,
+              );
+            },
+          ),
         ],
       );
 
   static void navigateTo(BuildContext context, String route) => context.go(route);
+  static void pop(BuildContext context) => context.pop();
+  static void push(BuildContext context, String route, {Object? extra}) => context.push(route, extra: extra);
 }
