a05c6823162956b5443461c0cc790854ec0779dc
MarioSQuilantan <mario.s@enhancefitness.com>
2023-03-19 23:38:24 -0600

    feat: :sparkles: Added character comics, events, series and stories blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:marvel_heroes_app/features/characters/presentation/bloc/characters_bloc.dart';
 
 import 'app_routes.dart';
 import 'core/theme/theme.dart';
+import 'features/character_comics/presentation/bloc/character_comics_bloc.dart';
+import 'features/character_events/presentation/bloc/character_events_bloc.dart';
+import 'features/character_series/presentation/bloc/character_series_bloc.dart';
+import 'features/character_stories/presentation/bloc/character_stories_bloc.dart';
+import 'features/characters/presentation/bloc/characters_bloc.dart';
 import 'injector.dart' as di;
 
 void main() {
@@ -19,6 +23,10 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<CharactersBloc>(create: (_) => di.locator<CharactersBloc>()),
+        BlocProvider<CharacterComicsBloc>(create: (_) => di.locator<CharacterComicsBloc>()),
+        BlocProvider<CharacterEventsBloc>(create: (_) => di.locator<CharacterEventsBloc>()),
+        BlocProvider<CharacterSeriesBloc>(create: (_) => di.locator<CharacterSeriesBloc>()),
+        BlocProvider<CharacterStoriesBloc>(create: (_) => di.locator<CharacterStoriesBloc>()),
       ],
       child: MaterialApp.router(
         title: 'Marvel Heroes',
